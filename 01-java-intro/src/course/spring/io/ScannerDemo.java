ddcfe475c4f0486f9f51d9842f773b6be080acf7
Trayan Iliev <office@iproduct.org>
2025-06-18 13:21:32 +0300

    Excpetion handling demo added

--- 01-java-intro/src/course/spring/io/ScannerDemo.java
+++ 01-java-intro/src/course/spring/io/ScannerDemo.java
@@ -1,23 +1,38 @@
 package course.spring.io;
 
+import course.spring.exception.CustomDBExcetion;
+
 import java.io.*;
 import java.util.Scanner;
 
-public class ScannerDemo  {
-    public static void main(String[] args) throws IOException {
-
+public class ScannerDemo {
+    public String readDataFromFile(String filename) throws IOException, CustomDBExcetion {
         Scanner s = null;
-
-        try (var br = new BufferedReader(new FileReader("./src/course/spring/io/ScannerDemo.java"))){
+        StringBuilder sb = new StringBuilder();
+        try (var br = new BufferedReader(new FileReader(filename))) {
             s = new Scanner(br);
             while (s.hasNext()) {
-                System.out.println(s.nextLine());
+                sb.append(s.nextLine());
             }
+        } catch (FileNotFoundException ex) {
+            System.out.printf("Catched in readData - '%s' does not exist: %s\n", filename, ex.getMessage());
+            throw new CustomDBExcetion("Error opening DB.", ex);
+//            System.out.println(ex);
+//            ex.printStackTrace();
         }
-//        finally {
-//            if (s != null) {
-//                s.close();
-//            }
-//        }
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String filename = "./src/course/spring/io/ScannerDemo2.java";
+        var demo = new ScannerDemo();
+        try {
+            demo.readDataFromFile(filename);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (CustomDBExcetion e) {
+            System.out.printf("Custom Ex. handler: %s, %s\n", e.getMessage(), e.getCause().getMessage());
+        }
     }
 }
