35be12911a5a8251ac6dfd46147bdf98c5b64289
Trayan Iliev <office@iproduct.org>
2025-06-23 09:28:31 +0300

    examples from spring project 2 -> project 3

--- 03-spring-gradle/src/main/java/course/spring/web/HelloRestController.java
+++ 03-spring-gradle/src/main/java/course/spring/web/HelloRestController.java
@@ -1,13 +1,42 @@
 package course.spring.web;
 
+import course.spring.dao.UserRepository;
+import course.spring.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/hello")
 public class HelloRestController {
-    @GetMapping(path = {"","/{name}"})
-    public String sayHello(@PathVariable(name = "name", required = false) String name,
-                           @RequestParam(value = "title", required = false, defaultValue = "") String title){
-        return "Hello "+ title + " " + name + ", from your REST Endpoint!";
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping({"", "{name}"})
+    public String sayHello(@PathVariable(name = "name", required = false) String name) {
+        return String.format("Hello %s, from Spring!", name != null ? name : "Guest");
     }
+
+
+    //http://localhost:8081/api/hello/params?
+    // from=01.01.2025&to=20.06.2025&doctor=Hristo%20Ivanov&patient=Ivan%20Petrov
+    @GetMapping("params")
+    public String sayHello(
+            @RequestParam(name = "from", required = false) String from,
+            @RequestParam(name = "to", required = false) String to,
+            @RequestParam(name = "doctor", required = false) String doctor,
+            @RequestParam(name = "patient", required = false) String patient
+    ) {
+        return String.format("From: %s<br>\n", from != null ? from : "not_present") +
+                String.format("To: %s<br>\n", to != null ? to : "not_present") +
+                String.format("Doctor: %s<br>\n", doctor != null ? doctor : "not_present") +
+                String.format("Patient: %s<br>\n", patient != null ? patient : "not_present");
+    }
+
+    @GetMapping("users")
+    public List<User> allUsers(){
+        return userRepo.findAll();
+    }
+
 }
