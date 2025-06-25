0f44859c6700e8a387a51b6ae1148445fc5ec582
Trayan Iliev <office@iproduct.org>
2025-06-25 13:32:32 +0300

    User CRUD + HelloRestController headers

--- 03-spring-gradle/src/main/java/course/spring/web/HelloRestController.java
+++ 03-spring-gradle/src/main/java/course/spring/web/HelloRestController.java
@@ -1,20 +1,23 @@
 package course.spring.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import course.spring.dao.UserRepository;
 import course.spring.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/hello")
 public class HelloRestController {
 
-    @GetMapping({"", "{name}"})
-    public String sayHello(@PathVariable(name = "name", required = false) String name) {
-        return String.format("Hello %s, from Spring!", name != null ? name : "Guest");
-    }
+    @Autowired
+    private ObjectMapper objectMapper;
 
     //http://localhost:8081/api/hello/params?
     // from=01.01.2025&to=20.06.2025&doctor=Hristo%20Ivanov&patient=Ivan%20Petrov
@@ -29,6 +32,21 @@ public class HelloRestController {
                 String.format("To: %s<br>\n", to != null ? to : "not_present") +
                 String.format("Doctor: %s<br>\n", doctor != null ? doctor : "not_present") +
                 String.format("Patient: %s<br>\n", patient != null ? patient : "not_present");
+    }
+
+    @GetMapping(value= "headers", produces = "text/html")
+    public String acceptHeaders(@RequestHeader("Accept") String accept) {
+        return String.format("<h1>Accept: %s</h1>", accept);
+    }
+
+    @GetMapping(value= "headers", produces = "application/json")
+    public String acceptHeaders(@RequestHeader MultiValueMap headers) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(headers.toSingleValueMap());
+    }
+
+    @GetMapping(value = {"", "{name}"}, produces = "text/html")
+    public String sayHello(@PathVariable(name = "name", required = false) String name) {
+        return String.format("Hello %s, from Spring!", name != null ? name : "Guest");
     }
 
 }
--- 03-spring-gradle/src/main/java/course/spring/web/UsersRestController.java
+++ 03-spring-gradle/src/main/java/course/spring/web/UsersRestController.java
@@ -3,6 +3,7 @@ package course.spring.web;
 import course.spring.dao.UserRepository;
 import course.spring.domain.UserService;
 import course.spring.dto.ErrorResponse;
+import course.spring.exception.InvalidEntityDataException;
 import course.spring.exception.NonexistingEntityException;
 import course.spring.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,22 @@ public class UsersRestController {
 //        return ResponseEntity.notFound().build();
 //    }
 
-    @GetMapping("{id}")
+    @GetMapping("{id:\\d+}")
     public User getUserById(@PathVariable("id") Long id) {
         return userService.getUserById(id);
     }
+
+    @GetMapping("count")
+    public long getUsersCount() {
+        return userService.getCount();
+    }
+
+    @GetMapping("/username/{username}")
+    public User getUserByUsername(@PathVariable("username") String username) {
+        return userService.getUserByUsername(username);
+    }
+
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -50,11 +63,33 @@ public class UsersRestController {
         ).body(newUser);
     }
 
+    @PutMapping("{id}")
+    public User updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+        if(!id.equals(user.getId())) {
+            throw new InvalidEntityDataException(
+                    String.format("Non-matching IDs in path '%s' and in request body '%s'.", id, user.getId())
+            );
+        }
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("{id}")
+    public User updateUser(@PathVariable("id") Long id) {
+        return userService.deleteUserById(id);
+    }
+
     @ExceptionHandler(NonexistingEntityException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(NonexistingEntityException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 new ErrorResponse(404, exception.getMessage(), null)
         );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleInvalidRequetData(InvalidEntityDataException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), ex.getViolations()));
     }
 
 }
